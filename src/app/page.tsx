c7aba7344bfed08754df3c135a39a6b75b0ceeb8
Pranav Mishra <pranavmishra2101@gmail.com>
2025-12-07 11:29:56 +0545

    resolved comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
-import { caller, getQueryClient, trpc } from "@/trpc/server";
+import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { Suspense } from "react";
 import { Client } from "./client";
 
 export default async function Home() {
   const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.createAi.queryOptions({ text: "CodexVibes" }));
-  const  data  = await caller.createAi({ text: "Hello, world!" });
+  void queryClient.prefetchQuery(
+    trpc.createAi.queryOptions({ text: "CodexVibes" })
+  );
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <Suspense fallback={<div>Loading...</div>}>
-       <Client/>
+        <Client />
       </Suspense>
-      </HydrationBoundary>
+    </HydrationBoundary>
   );
 }

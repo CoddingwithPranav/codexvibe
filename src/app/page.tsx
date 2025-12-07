9509ea9e9d671fd7dae510e148456a6b1f94f5b8
Pranav Mishra <pranavmishra2101@gmail.com>
2025-12-07 18:06:59 +0545

    04 background job

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
-import { getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
-import { Client } from "./client";
+"use client";
 
-export default async function Home() {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(
-    trpc.createAi.queryOptions({ text: "CodexVibes" })
-  );
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+
+export default  function Home() {
+  const trpc = useTRPC();
+
+  const invoke = useMutation(trpc.inoke.mutationOptions({}))
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Client />
-      </Suspense>
-    </HydrationBoundary>
+    <div className="p-4 max-w-7xl mx-auto">
+      <Button onClick={()=>{invoke.mutate({name:"pranav"})}} variant="default">Invoke Background Job</Button>
+
+    </div>
   );
 }
